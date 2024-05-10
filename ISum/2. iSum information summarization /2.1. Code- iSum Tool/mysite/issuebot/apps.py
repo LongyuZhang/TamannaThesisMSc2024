9aaf39f5956aef63ef64fc0d5cd85b0c6d6acd02
Tamanna <tamannacsebuet@gmail.com>
2024-05-10 07:33:03 -0600

    rename

--- ISum/2. iSum information summarization /2.1. Code- iSum Tool/mysite/issuebot/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IssueBotConfig(AppConfig):
-    name = 'issuebot'
--- ISum/2. iSum information summarization /2.1. Code- iSum Tool/mysite/issuebot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    re_path(r'^get_issue_info/$', views.get_issue_info, name='get_issue_info'),
-]
